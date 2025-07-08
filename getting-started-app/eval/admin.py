8868190102218a03de8547b8debb2f4ca3d4aec9
hyunggyun-kim <32211307@dankook.ac.kr>
2025-07-08 18:46:16 +0900

    관리자 평균 점수 기준 프로젝트 정렬하여 확인
    평균 점수를 기준으로 프로젝트를 정렬하는 기능 추가

--- getting-started-app/eval/admin.py
+++ getting-started-app/eval/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Project, Star
+from django.db.models import Avg
 
-# Register your models here.
-admin.site.register(Project)
-admin.site.register(Star)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('project', 'explain', 'average_score')
+
+    def average_score(self, obj):
+        return obj.average()
+    
+    average_score.short_description = '평균 점수'
+    average_score.admin_order_field = 'average_sort_key'
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(average_sort_key=Avg('stars__score'))
+    
